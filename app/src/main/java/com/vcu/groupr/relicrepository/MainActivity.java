37c0843b6e9c673b2536c91b0b0804ddad327228
Ryan Maybush <maybushcr@mymail.vcu.edu>
2018-04-25 19:08:38 -0400

    checksums.ser

--- app/src/main/java/com/vcu/groupr/relicrepository/MainActivity.java
+++ app/src/main/java/com/vcu/groupr/relicrepository/MainActivity.java
@@ -8,9 +8,9 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
-import com.firebase.ui.auth.AuthUI;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.firebase.ui.auth.AuthUI;
 import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.Arrays;
